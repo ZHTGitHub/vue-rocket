fccc054c38c7208ce9eade8b2d084b9748e6e4fa
ZHTGitHub <chat195@163.com>
2021-09-17 10:45:35 +0800

    完善首页

--- examples/views/Home/ZHome.vue
+++ examples/views/Home/ZHome.vue
@@ -4,7 +4,7 @@
       <h4 class="mb-8 text-h4">{{ $route.meta.title }}</h4>
       
       <h6 class="text-h5">介绍</h6>
-      <p class="mb-12 intro"><a target="_blank" href="">Vue Rocket</a> 是一个后台前端解决方案，它基于 <a target="_blank" href="https://cn.vuejs.org/index.html">vue</a> 和 <a target="_blank" href="https://vuetifyjs.com/">vuetify</a> 实现。它使用了最新的前端技术栈，对表单进行全局统一处理，提供了丰富的全局样式，它可以帮助你快速搭建企业级中后台产品原型。</p>
+      <p class="mb-12 intro"><a target="_blank" href="/">Vue Rocket</a> 是一个后台前端解决方案，它基于 <a target="_blank" href="https://cn.vuejs.org/index.html">vue</a> 和 <a target="_blank" href="https://vuetifyjs.com/">vuetify</a> 实现。它使用了最新的前端技术栈，对表单进行全局统一处理，提供了丰富的全局样式，它可以帮助你快速搭建企业级中后台产品原型。</p>
     
       <h6 class="text-h5">前序准备</h6>
       <p class="mb-12">
@@ -23,30 +23,32 @@
         <a target="_blank" href="https://vuetifyjs.com/">vuetify</a>，提前了解和学习这些知识会对使用本项目有很大的帮助。</p>
 
         <h6 class="text-h5">安装</h6>
-        <v-subheader>使用 vue-rocket 前需预安装vue、vue-router、vuex、vuetify、lodash，否则项目无法正常运行。</v-subheader>
-        <pre class="rounded-md line-numbers"><code class="language-bash">{{ install }}</code></pre>
+        <v-subheader>使用&nbsp;<a target="_blank" href="/"> Vue Rocket</a>&nbsp;前需预安装&nbsp;
+          <a target="_blank" href="https://cn.vuejs.org/index.html">vue</a>、
+          <a target="_blank" href="https://vuex.vuejs.org/zh-cn/">vuex</a>、
+          <a target="_blank" href="https://vuetifyjs.com/">vuetify</a>、
+          <a target="_blank" href="https://www.lodashjs.com/">lodash</a>，
+          否则项目无法正常运行。</v-subheader>
+        <code>
+          npm i vue-rocket
+        </code>
+        <p>安装完 vue-rocket 需在项目的状态管理引入管理表单的 store</p>
+        
+        <div class="pl-4 pb-12">
+          <img src="../../../public/store.jpg">
+        </div>
     </main-layout>
   </div>
 </template>
 
 <script>
-  import Prism from 'prismjs'
-  import 'prismjs/themes/prism-okaidia.css'
-
-  const install = ` # 安装 vue-rocket
-    npm install vue-rocket`
-
   export default {
     name: 'ZHome',
 
     data() {
       return {
-        install
+        
       }
-    },
-
-    mounted() {
-      Prism.highlightAll()
     },
 
     components: {
@@ -69,6 +71,11 @@
     a {
       color: #67c23a;
       text-decoration: none;
+    }
+
+    code {
+      display: block;
+      margin-left: 16px;
     }
   }
 </style>
