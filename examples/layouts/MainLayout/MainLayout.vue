95c0661c7de59d7c600515db880dd3b4bf85efbc
ZHTGitHub <chat195@163.com>
2021-09-17 14:01:52 +0800

    优化app-bar

--- examples/layouts/MainLayout/MainLayout.vue
+++ examples/layouts/MainLayout/MainLayout.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="main-layout">
-    <v-app-bar app elevation="1" clipped-left style="z-index: 7">
-      <v-app-bar-nav-icon @click="onToggle"></v-app-bar-nav-icon>
-      <v-toolbar-title>Vue Rocket</v-toolbar-title>
-    </v-app-bar>
+      <v-app-bar 
+        app 
+        color="#fcb69f"
+        dark
+        clipped-left 
+        elevate-on-scroll
+        src="https://picsum.photos/1920/1080?random"
+        style="z-index:7"
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+
+        <v-app-bar-nav-icon @click="onToggle"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Vue Rocket</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-export</v-icon>
+        </v-btn>
+      </v-app-bar>
 
     <z-drawer 
       ref="drawer"
