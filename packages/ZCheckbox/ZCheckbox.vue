800f21852e1a805c7f5fe43366a968e555551552
ZHTGitHub <chat195@163.com>
2022-01-17 09:02:00 +0800

    ci: 开发checkbox组件

--- packages/ZCheckbox/ZCheckbox.vue
+++ packages/ZCheckbox/ZCheckbox.vue
@@ -2,14 +2,23 @@
   <div class="z-checkbox z-input" :style="{ width: breadth }">
     <v-checkbox
       v-model="value"
+      :color="color"
       :dense="dense"
       :disabled="disabled"
       :error="incorrect"
       :error-messages="errorMessage"
+      :false-value="falseValue"
       :hide-details="hideDetails"
+      :indeterminate="indeterminate"
+      :indeterminate-icon="indeterminateIcon"
       :label="label"
+      :off-icon="offIcon"
+      :on-icon="onIcon"
+      :persistent-hint="persistentHint"
+      :prepend-icon="prependIcon"
       :readonly="readonly"
       :success="success"
+      :true-value="trueValue"
       :value="customValue"
       @change="onChange"
       @click="onClick"
@@ -25,10 +34,55 @@
     mixins: [FormMixins],
 
     props: {
+      color: {
+        type: String,
+        required: false
+      },
+
       customValue: {
         type: [String, Number],
         required: false
-      }
+      },
+
+      falseValue: {
+        type: [String, Number, Boolean, undefined, null, NaN, Symbol, Object, Array],
+        required: false
+      },
+
+      indeterminate: {
+        type: Boolean,
+        default: false
+      },
+
+      indeterminateIcon: {
+        type: String,
+        required: false
+      },
+
+      offIcon: {
+        type: String,
+        required: false
+      },
+
+      onIcon: {
+        type: String,
+        required: false
+      },
+
+      persistentHint: {
+        type: String,
+        required: false
+      },
+
+      prependIcon: {
+        type: String,
+        required: false
+      },
+
+      trueValue: {
+        type: [String, Number, Boolean, undefined, null, NaN, Symbol, Object, Array],
+        required: false
+      },
     },
 
     data() {
