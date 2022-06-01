7052c1fb3b5a55235bad561830bfcafbdb982b52
ZHTGitHub <chat195@163.com>
2022-06-01 15:43:42 +0800

    ci: 修改地址

--- examples/views/Home/ZHome.vue
+++ examples/views/Home/ZHome.vue
@@ -5,7 +5,12 @@
         <typing-title :title="$route.meta.title"></typing-title>
       
         <h6 class="text-h5">介绍</h6>
-        <p class="mb-12 intro"><a target="_blank" href="/">Vue Rocket</a> 是一个后台前端解决方案，它基于 <a target="_blank" href="https://cn.vuejs.org/index.html">vue2.x</a> 和 <a target="_blank" href="https://vuetifyjs.com/">vuetify</a> 实现。对表单进行全局统一处理，提供了丰富的全局样式，它可以帮助你快速搭建企业级中后台产品原型。</p>
+        <p class="mb-12 intro">
+          <a target="_blank" href="https://www.npmjs.com/package/vue-rocket">Vue Rocket</a> 
+          是一个后台前端解决方案，它基于 
+          <a target="_blank" href="https://cn.vuejs.org/index.html">vue</a> 和
+          <a target="_blank" href="https://vuetifyjs.com/">vuetify</a> 
+          实现；统一管理表单数据，常用JS方法封装，提供丰富的全局样式；它可以帮助你快速搭建企业级中后台产品。</p>
       
         <h6 class="text-h5">前序准备</h6>
         <p class="mb-12">
@@ -24,7 +29,7 @@
           <a target="_blank" href="https://vuetifyjs.com/">vuetify</a>，提前了解和学习这些知识会对使用本项目有很大的帮助。</p>
 
           <h6 class="text-h5">安装</h6>
-          <v-subheader>使用&nbsp;<a target="_blank" href="/"> Vue Rocket</a>&nbsp;前需预安装&nbsp;
+          <v-subheader>使用&nbsp;<a target="_blank" href="https://www.npmjs.com/package/vue-rocket">Vue Rocket</a>&nbsp;前需预安装&nbsp;
             <a target="_blank" href="https://cn.vuejs.org/index.html">vue</a>、
             <a target="_blank" href="https://vuex.vuejs.org/zh-cn/">vuex</a>、
             <a target="_blank" href="https://vuetifyjs.com/">vuetify</a>， 
