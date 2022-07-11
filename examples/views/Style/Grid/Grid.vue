af924608170fbd054e98f2e8f798132b6e5cfc7a
ZHTGitHub <chat195@163.com>
2022-07-11 14:50:45 +0800

    ci: 开发全局样式

--- examples/views/Style/Grid/Grid.vue
+++ examples/views/Style/Grid/Grid.vue
@@ -3,51 +3,51 @@
     <typing-title :title="$route.meta.title"></typing-title>
 
     <div class="mb-8">
-      <div class="row">
-        <div class="col-24">
-          <div class="text-center bg-blue box">col-24</div>
+      <div class="z-row">
+        <div class="z-col-24">
+          <div class="text-center bg-blue box">z-col-24</div>
         </div>
       </div>
     </div>
 
     <div class="mb-8">
-      <div class="row">
-        <div class="col-12">
-          <div class="text-center bg-cyan box">col-12</div>
+      <div class="z-row">
+        <div class="z-col-12">
+          <div class="text-center bg-cyan box">z-col-12</div>
         </div>
-        <div class="col-12">
-          <div class="text-center bg-blue box">col-12</div>
-        </div>
-      </div>
-    </div>
-
-    <div class="mb-8">
-      <div class="row">
-        <div class="col-8">
-          <div class="text-center bg-cyan box">col-8</div>
-        </div>
-        <div class="col-8">
-          <div class="text-center bg-blue box">col-8</div>
-        </div>
-        <div class="col-8">
-          <div class="text-center bg-cyan box">col-8</div>
+        <div class="z-col-12">
+          <div class="text-center bg-blue box">z-col-12</div>
         </div>
       </div>
     </div>
 
     <div class="mb-8">
-      <div class="row">
-        <div class="col-6">
-          <div class="text-center bg-cyan box">col-6</div>
+      <div class="z-row">
+        <div class="z-col-8">
+          <div class="text-center bg-cyan box">z-col-8</div>
         </div>
-        <div class="col-6">
-          <div class="text-center bg-blue box">col-6</div>
+        <div class="z-col-8">
+          <div class="text-center bg-blue box">z-col-8</div>
         </div>
-        <div class="col-6">
-          <div class="text-center bg-cyan box">col-6</div>
+        <div class="z-col-8">
+          <div class="text-center bg-cyan box">z-col-8</div>
         </div>
-        <div class="col-6">
-          <div class="text-center bg-blue box">col-6</div>
+      </div>
+    </div>
+
+    <div class="mb-8">
+      <div class="z-row">
+        <div class="z-col-6">
+          <div class="text-center bg-cyan box">z-col-6</div>
+        </div>
+        <div class="z-col-6">
+          <div class="text-center bg-blue box">z-col-6</div>
+        </div>
+        <div class="z-col-6">
+          <div class="text-center bg-cyan box">z-col-6</div>
+        </div>
+        <div class="z-col-6">
+          <div class="text-center bg-blue box">z-col-6</div>
         </div>
       </div>
     </div>
